02e615227c8be74d3367dad556b8cb0bf102001f
ruedasan <125039095+ruedasan@users.noreply.github.com>
2023-11-17 14:56:47 -0500

    Add files via upload

---
+++ Magnetometro.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+#include <Wire.h> //I2C Librería de Arduino 
+#define address 0x1E //0011110b, I2C 7bit address of HMC5883
+
+void setup(){
+  Serial.begin(9600);
+  Wire.begin();
+  
+  Wire.beginTransmission(address); //open communication with HMC5883
+  Wire.write(0x02); //select mode register
+  Wire.write(0x00); //continuous measurement mode
+  Wire.endTransmission();
+}
+
+void loop(){
+  
+  int x,y,z; //datos de datos triples
+  int xmin,xmax,ymin,ymax,zmin,zmax;
+  xmin=0; xmax=0; ymax=0; ymin = 0; zmin=0;zmax=0;
+  //Tell the HMC5883 where to begin reading data
+  Wire.beginTransmission(address);
+  Wire.write(0x03); //seleccionar registro, 3 X MSB 
+  Wire.endTransmission();
+  
+ 
+ //Leer datos de cada eje, son dos registros por eje
+  Wire.requestFrom(address, 6);
+  if(6<=Wire.available()){
+    x = Wire.read()<<8; //X msb
+    x |= Wire.read(); //X lsb
+    z = Wire.read()<<8; //Z msb
+    z |= Wire.read(); //Z lsb
+    y = Wire.read()<<8; //Y msb
+    y |= Wire.read(); //Y lsb
+  }
+  
+  //Imprime valores para cada eje
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print("  y: ");
+  Serial.print(y);
+  Serial.print("  z: ");
+  Serial.println(z);
+  
+  delay(250);
+}
