0aa0ab713ed5b600bfd369274453e82b4e695390
ruedasan <125039095+ruedasan@users.noreply.github.com>
2023-11-22 21:21:20 -0500

    Add files via upload

---
+++ Funcional/GPS.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <TinyGPS++.h>
+
+#define RXD2 16
+#define TXD2 17
+HardwareSerial neogps(1);
+
+void print_speed();
+
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+}
+
+void loop() {
+  boolean newData = false;
+  for (unsigned long start = millis(); millis() - start < 1000;) {
+    while (neogps.available()) {
+      if (gps.encode(neogps.read())) {
+        newData = true;
+      }
+    }
+  }
+
+  if (newData) {
+    newData = false;
+    Serial.println(gps.satellites.value());
+    print_speed();
+  } else {
+    Serial.println("No Data");
+  }
+}
+
+void print_speed() {
+  if (gps.location.isValid()) {
+    Serial.print("Lat: ");
+    Serial.println(gps.location.lat(), 6);
+    
+    Serial.print("Lng: ");
+    Serial.println(gps.location.lng(), 6);
+
+    Serial.print("Speed: ");
+    Serial.println(gps.speed.kmph());
+
+    Serial.print("SAT: ");
+    Serial.println(gps.satellites.value());
+
+    Serial.print("ALT: ");
+    Serial.println(gps.altitude.meters(), 0);
+  } else {
+    Serial.println("No Data");
+  }
+}
